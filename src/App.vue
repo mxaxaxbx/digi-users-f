686005a8563ac951118ae4987990031b94d8a5c2
Miguel A. Arenas <6446220+mxaxaxbx@users.noreply.github.com>
2024-09-27 13:27:35 -0500

    Initialize qa

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <NavBar/>
+  <router-view/>
+</template>
+
+<script setup lang="ts">
+import { defineAsyncComponent } from 'vue';
+
+const NavBar = defineAsyncComponent(() => import('./components/global/nav-bar.vue'));
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="home">
+    <p class="bg-red-500">holaaa</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
