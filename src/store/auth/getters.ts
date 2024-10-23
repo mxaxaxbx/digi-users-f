812cb0db32d99d412e267abead2e61becf8d0c51
Miguel A Arenas <miguel.arenas@sitidoctor.com>
2024-10-23 10:21:49 -0500

    Refactor auth getters to use localStorage for user data

--- src/store/auth/getters.ts
+++ src/store/auth/getters.ts
@@ -1,10 +1,19 @@
 import { GetterTree } from 'vuex';
-import { AuthStateI } from './state';
+
+import { decode } from '@/utils/custom-enc-dec';
+
+import { AuthStateI, UserI } from './state';
 import { RootStateI } from '../state';
 
 export const getters: GetterTree<AuthStateI, RootStateI> = {
   isAuthenticated: (state) => {
-    const { token } = state;
-    return !!token;
+    const user = localStorage.getItem('user');
+    return !!user;
+  },
+  user: (state) => {
+    const encodedUser = localStorage.getItem('user');
+    if (!encodedUser) return null;
+    const { value } = decode(encodedUser);
+    return value as UserI;
   },
 };
