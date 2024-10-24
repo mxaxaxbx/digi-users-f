f3a67e017f3c03ae63e34d70192e7ebb71e21105
Miguel A Arenas <miguel.arenas@sitidoctor.com>
2024-10-23 20:50:06 -0500

    Refactor login action to include app parameter in redirect

--- src/views/app/index.vue
+++ src/views/app/index.vue
@@ -1,6 +1,47 @@
 <template>
   <div>
     <h1>App</h1>
+    <p v-if="loading">Loading...</p>
     <router-view></router-view>
   </div>
 </template>
+
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+const loading = ref(false);
+
+function switchApp() {
+  const app = route.query.app as string;
+  if (!app) {
+    return;
+  }
+
+  const businesses = localStorage.getItem('businesses');
+  const permissions = localStorage.getItem('permissions');
+  const token = localStorage.getItem('token');
+  const user = localStorage.getItem('user');
+
+  loading.value = true;
+  switch (app) {
+    case 'edu': {
+      const eduApp = process.env.VUE_APP_URL_DG_EDU_APP;
+      window.location.href = `${eduApp}/auth/confirmsession?token=${token}&user=${user}&businesses=${businesses}&permissions=${permissions}`;
+      break;
+    }
+    case 'care': {
+      const careApp = process.env.VUE_APP_URL_DG_CARE_APP;
+      window.location.href = `${careApp}/auth/confirmsession?token=${token}&user=${user}&businesses=${businesses}&permissions=${permissions}`;
+      break;
+    }
+    default:
+      break;
+  }
+}
+
+onMounted(() => {
+  switchApp();
+});
+</script>
