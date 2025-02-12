67d76acbe43d2b6dbbb9dfcb33fa8eec8cd8123f
mxaxaxbx <6446220+mxaxaxbx@users.noreply.github.com>
2025-02-12 10:24:23 -0500

    Enhance edit profile view with custom form component and initial field setup

--- src/views/app/users/edit-profile.vue
+++ src/views/app/users/edit-profile.vue
@@ -1,3 +1,34 @@
 <template>
-  edit-profile
+  <div class="bg-white p-5 rounded-lg shadow-lg">
+    <h1 class="text-2xl font-bold mb-5">Edit Profile</h1>
+    <!-- line -->
+    <div class="mb-5">
+      <hr />
+    </div>
+    <Form
+      :fields="fields"
+      :loading="loading"
+    />
+  </div>
 </template>
+
+<script setup lang="ts">
+import { defineAsyncComponent, ref } from 'vue';
+
+import { CustomFormStateI } from '@/store/custom-form/state';
+
+const Form = defineAsyncComponent(() => import('@/components/global/customform.vue'));
+
+const loading = ref(false);
+const fields = ref<CustomFormStateI[]>([
+  {
+    name: 'first-name',
+    value: '',
+    type: 'text',
+    label: 'First Name',
+    required: true,
+    rules: 'min:3,max:50',
+  },
+]);
+
+</script>
