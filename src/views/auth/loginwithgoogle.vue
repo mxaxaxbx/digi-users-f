3b92838ef0c2b9b62e0260287073e106505ef1e5
mxaxaxbx <6446220+mxaxaxbx@users.noreply.github.com>
2025-02-19 11:03:46 -0500

    Add Google login functionality with dynamic URI generation in login component

--- src/views/auth/loginwithgoogle.vue
+++ src/views/auth/loginwithgoogle.vue
@@ -24,6 +24,28 @@
         <span class="ml-2 font-bold">Login with Google</span>
       </span>
     </button>
+    <a
+      :href="`https://accounts.google.com/o/oauth2/v2/auth?${uriquery}`"
+      class="
+        block
+        bg-white
+        px-4 py-4
+        rounded-lg shadow-md border border-gray-300
+        hover:border-gray-400
+        focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-opacity-50
+        w-full
+      "
+    >
+      <span class="flex items-center">
+        <img
+          src="https://img.icons8.com/color/48/000000/google-logo.png"
+          alt="google"
+          class="w-6 h-6"
+        />
+        <!-- <i class="fab fa-google text-2xl"></i> -->
+        <span class="ml-2 font-bold">Login with Google</span>
+      </span>
+    </a>
   </div>
 </template>
 
@@ -46,6 +68,7 @@ const SCOPES = 'https://www.googleapis.com/auth/userinfo.email https://www.googl
 const route = useRoute();
 const router = useRouter();
 const token = ref<string>('');
+const uriquery = ref<string>('');
 
 function redirectToApp() {
   const { app, redirect } = route.query;
@@ -112,8 +135,24 @@ function loadGis() {
   });
 }
 
+function generateUri() {
+  const queryObject: any = {
+    response_type: 'code',
+    client_id: process.env.VUE_APP_GOOGLE_CLIENT_ID,
+    redirect_uri: `${process.env.VUE_APP_DIGI_USERS_F}/auth/validategoogletoken`,
+    scope: SCOPES,
+    access_type: 'offline',
+    prompt: 'consent',
+  };
+
+  uriquery.value = Object.keys(queryObject)
+    .map((key) => `${encodeURIComponent(key)}=${encodeURIComponent(queryObject[key])}`)
+    .join('&');
+}
+
 onMounted(async () => {
   try {
+    generateUri();
     await loadScript('https://apis.google.com/js/api.js');
     loadGapi();
     await loadScript('https://accounts.google.com/gsi/client');
