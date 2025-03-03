403d4a9845c88b0d8b02a21e82f9b21dcb3b8579
mxaxaxbx <6446220+mxaxaxbx@users.noreply.github.com>
2025-03-03 11:23:25 -0500

    Refactor users-client to use a single environment variable for Digi Users URL

--- src/http-client/users-client.ts
+++ src/http-client/users-client.ts
@@ -3,8 +3,7 @@ import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
 import { camelToSnake } from '@/utils/index';
 import { decode } from '@/utils/custom-enc-dec';
 
-const ENVIRONMENT = process.env.VUE_APP_ENVIRONMENT ?? 'DEV';
-const URL_DIGIUSERS = process.env[`VUE_APP_URL_DG_USERS_${ENVIRONMENT}`];
+const URL_DIGIUSERS = process.env.VUE_APP_URL_DG_USERS;
 
 const baseHttpClient = axios.create({
   baseURL: URL_DIGIUSERS,
