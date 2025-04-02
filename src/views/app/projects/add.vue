b0dc17653c44f01bed4aaf73eceef30f6c2c3793
mxaxaxbx <6446220+mxaxaxbx@users.noreply.github.com>
2025-04-02 12:30:37 -0500

    Add project name input field to project addition view

--- src/views/app/projects/add.vue
+++ src/views/app/projects/add.vue
@@ -1,3 +1,13 @@
 <template>
-  holis
+  holis {{ projectName }}
+  <dgInput v-model="projectName"/>
 </template>
+
+<script setup lang="ts">
+import { defineAsyncComponent, ref } from 'vue';
+
+const dgInput = defineAsyncComponent(() => import('@/components/global/dginput.vue'));
+
+const projectName = ref('');
+
+</script>
