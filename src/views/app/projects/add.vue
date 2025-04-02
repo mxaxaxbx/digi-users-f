8416d05a393ac6c11ba8d0b480fddffb385ae11f
mxaxaxbx <6446220+mxaxaxbx@users.noreply.github.com>
2025-04-02 15:22:48 -0500

    Add submit button to project addition view with loading state

--- src/views/app/projects/add.vue
+++ src/views/app/projects/add.vue
@@ -14,6 +14,20 @@
         Project Name
       </label>
       <dgInput v-model="projectName" name="projectName"/>
+      <div class="flex items-center justify-end mt-4">
+        <button
+          type="submit"
+          class="
+            px-4 py-2
+            text-sm font-medium text-white bg-pink-600
+            rounded-md hover:bg-pink-700 focus:outline-none focus:ring-2 focus:ring-pink-500
+          "
+          :disabled="loading || !projectName"
+        >
+          <i v-if="loading" class="fa fa-spinner fa-spin mr-2"></i>
+          Create Project
+        </button>
+      </div>
     </form>
   </div>
 </template>
@@ -28,10 +42,15 @@ import {
 const dgInput = defineAsyncComponent(() => import('@/components/global/dginput.vue'));
 
 const projectName = ref('');
+const loading = ref(false);
 
 function generateRandomName() {
   const randomNumber = Math.floor(Math.random() * 10000);
   projectName.value = `My Project ${randomNumber}`;
+}
+
+function submit() {
+  console.log('Project Name:', projectName.value);
 }
 
 onMounted(() => {
